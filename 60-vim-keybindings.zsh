08553fe3b1025fbb712931cc862de0d69869851d
royhan azhar <roihanazhar24@gmail.com>
2026-09-16 18:42:51 +0700

    Fix cursor shape for Vim mode (block) and Insert mode (beam)

--- 60-vim-keybindings.zsh
+++ 60-vim-keybindings.zsh
@@ -2,27 +2,36 @@ bindkey -v
 export KEYTIMEOUT=1
 setopt PROMPT_SUBST
 
-# Status Indicator Function
+# Status Indicator Text
 VIM_MODE_TXT="%F{#ff5555}%B[ VIM ]%b%f"
 INS_MODE_TXT="%F{#8be9fd}%B[ INSERT ]%b%f"
 CURRENT_VIM_STATUS=$INS_MODE_TXT
 
+# Ubah bentuk kursor berdasarkan mode (Block di Vim Mode, Pipe | di Insert Mode)
 function zle-keymap-select {
   if [[ $KEYMAP == vicmd ]]; then
     CURRENT_VIM_STATUS=$VIM_MODE_TXT
+    print -n "\e[2q" # Solid BlockCursor [█]
   else
     CURRENT_VIM_STATUS=$INS_MODE_TXT
+    print -n "\e[6q" # Beam/Pipe Cursor [|]
   fi
   zle reset-prompt
 }
 zle -N zle-keymap-select
+
+# Pastikan kursor kembali ke Pipe | saat prompt baru muncul
+function zle-line-init {
+  print -n "\e[6q"
+}
+zle -N zle-line-init
 
 # Native Zsh History Search by Prefix
 autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
 
-# Insert Mode Bindings (Tetap berada di Insert Mode)
+# Insert Mode Bindings
 bindkey '^[[A' up-line-or-beginning-search
 bindkey '^[[B' down-line-or-beginning-search
 bindkey '^K' up-line-or-beginning-search
@@ -38,7 +47,7 @@ bindkey -M vicmd '^[[B' down-line-or-beginning-search
 bindkey -M vicmd 'K' up-line-or-history
 bindkey -M vicmd 'J' down-line-or-history
 
-# Izinkan Backspace menghapus karakter di Command Mode (Vicmd)
+# Allow Backspace & x in Command Mode
 bindkey -M vicmd '^?' backward-delete-char
 bindkey -M vicmd '^H' backward-delete-char
 bindkey -M vicmd 'x' delete-char
